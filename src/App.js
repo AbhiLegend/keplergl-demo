3970cff3c8697e4528ee407a75f985740c1a0fcd
Abhishek Nandy <abhishek.nandy81@gmail.com>
2020-08-31 18:55:17 -0400

    keplergldemo
    update

--- src/App.js
+++ src/App.js
@@ -24,9 +24,7 @@ export default function App() {
 function Map() {
   const dispatch = useDispatch();
   const { data } = useSwr("covid", async () => {
-    const response = await fetch(
-      "https://gist.githubusercontent.com/leighhalliday/a994915d8050e90d413515e97babd3b3/raw/a3eaaadcc784168e3845a98931780bd60afb362f/covid19.json"
-    );
+    const response = await fetch("C:\rect\keplergl-demo\prescribersd.csv");
     const data = await response.json();
     return data;
   });
@@ -55,7 +53,7 @@ function Map() {
   return (
     <KeplerGl
       id="covid"
-      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_API}
+      mapboxApiAccessToken={"pk.eyJ1IjoiYWJoaWNsb3Vkc3RhbGsiLCJhIjoiY2tlMDc5Zm5xMXZnejJxcGlkaXpkZzUwNSJ9.kWeRxrUWOhZ4CfsLbricwQ"}
       width={window.innerWidth}
       height={window.innerHeight}
     />
